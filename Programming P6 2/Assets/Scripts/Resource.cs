6a9f9067279c74eb66fb04b1e50bf2b95fb1f185
MikeyBuchan <psmbgames@hotmail.com>
2019-01-25 22:15:56 +0100

    sad

---
+++ Programming P6 2/Assets/Scripts/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour 
+{
+    public string resourceName;
+    public float itemValue;
+    
+    public virtual void ItemInfo()
+    {
+
+    }
+}
---
+++ Programming P6 2/Assets/Scripts/Rock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : Resource 
+{
+    public bool hasIron;
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+}
